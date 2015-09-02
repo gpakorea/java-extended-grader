58aa4d9d713a1f274da0eaa4c5ac6503b5e89594
Aaron Snowberger <jekkilekki@gmail.com>
2015-09-02 13:54:52 +0900

    Deleted a bunch of stuff

--- ExtendedGrader.java
+++ ExtendedGrader.java
@@ -35,68 +35,6 @@ class ExtendedGrader {
       
     }
     
-    /*
-     * Challenge: Create an if-else that tells the user "Congrats!"
-     * if their grade is greater than 100 (bonus points). OR
-     * "You're a normal student." if under 100.
-     */
-    if ( grade == 100 ) {
-      System.out.println( "Congrats! Perfect score!" );
-    } else if ( grade > 100 ) {
-      System.out.println( "You got bonus points, right?" );
-    } else {
-      System.out.println( "You're a normal student." );
-    }
-    
-    /*
-     * Challenge: Using an if-elseif-else, tell the user whether
-     * their grade is "positive" or "negative" or "zero".
-     */
-    if ( grade == 0 ) {
-      System.out.println( "You got a zero." );
-    } else if ( grade > 0 ) {
-      System.out.println( "Your number is positive." );
-    } else {
-      System.out.println( "Your number is negative." );
-    }
-    
-    /*
-     * Challenge: Make the computer print EXACTLY the following: 
-     * 
-     * From: Bill Smith
-     * Address: Dell Computer, Bldg 13
-     * Date: April 12, 2005
-     * 
-     * To: Jack Jones
-     * 
-     * Message: Help! I’m trapped inside a computer!
-     */
-    
-    // Using multiple System.outs
-    System.out.println( "METHOD 1:" );
-    System.out.println( "" );
-    System.out.println( "From: Bill Smith" );
-    System.out.println( "Address: Dell Computer, Bldg 13" );
-    System.out.println( "Date: April 12, 2005" );
-    System.out.println( "" );
-    System.out.println( "To: Jack Jones" );
-    System.out.println( "" );
-    System.out.println( "Message: Help! I'm trapped inside a computer!" );
-    System.out.println( "" );
-    
-    // Using String assignments and concatenation
-    String from = "From: Bill Smith";
-    String address = "Address: Dell Computer, Bldg 13";
-    String date = "Date: April 12, 2005";
-    String to = "To: Jack Jones";
-    String message = "Message: Help! I'm trapped inside a computer!";
-    
-    System.out.println( "METHOD 2:\n\n" + from + "\n" + address + "\n" + date + "\n\n" + to + "\n\n" + message + "\n" );
-    
-    // Using ONE line of code
-    System.out.println( "METHOD 3:\n\nFrom: Bill Smith\nAddress: Dell Computer, Bldg 13\nDate: April 12, 2005\n\nTo: Jack Jones\n\nMessage: Help! I'm trapped inside a computer!\n" );
-    
-    
     // Showing new commits
     System.out.println( "Modifying this program for GitHub.com" );
     
